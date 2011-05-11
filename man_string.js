c111f3884aef0f28c4c437036119c3ba41e2c3d1
Surendrans <surendrans86@gmail.com>
2011-05-12 01:06:04 +0530

    added 5 more methods strip,start_with, upcase,first,last

--- man_string.js
+++ man_string.js
@@ -1,5 +1,12 @@
 var s_ = { 
 capitalize : function (val) {return val.replace(val[0], val[0].toUpperCase())},
-chomp : function (val, replace) {var regex = replace ? new RegExp(replace + "$") : /\n\r|\n|\r$/; return val.replace(regex, "");}
+chomp : function (val, replace) {var regex = replace ? new RegExp(replace + "$") : /\n\r|\n|\r$/; return val.replace(regex, "");},
+strip : function (val) { return val.replace(/^\s*/, '').replace(/\s*$/, ''); },
+start_with : function (val,match) { var str = val.match(new RegExp("^" + match)); return  str ? true : false },
+upcase : function (val) { return val.toUpperCase();},
+first : function (val) { return val[0];},
+last : function (val) { return val[val.length-1];}
 };
+
+
 
